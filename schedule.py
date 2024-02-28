15b574f1a6e647af86cf3eb599ebee3e67b36c18
Amir Mohseni <64486276+Amirmohseni1@users.noreply.github.com>
2024-02-29 00:42:24 +0330

    add classes and logger

---
+++ schedule.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger("Schedule")
+
+
+class Scheduler:
+    def __init__(self):
+        self.jobs = []
+
+
+class Job:
+    def __init__(self):
+        pass
